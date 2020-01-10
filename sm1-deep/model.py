f86bb5882a6d837f285de598a5a2666db9c7897e
Bassel Mahmoud <dev.bma@outlook.com>
2020-01-10 20:12:35 +0100

    implement the model
    Signed-off-by: Bassel Mahmoud <dev.bma@outlook.com>

---
+++ sm1-deep/model.py
@@ -0,0 +1,28 @@
+from tensorflow.python.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg16 import VGG16
+
+
+def create_model():
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
+    preds = Dense(3, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=preds)
+
+    for i, layer in enumerate(model.layers):
+        print(i, layer.name)
+
+    # Fix the weight fo the first 20 layers
+    for layer in model.layers[:20]:
+        layer.trainable = False
+
+    for layer in model.layers[20:]:
+        layer.trainable = True
+
+    return model
