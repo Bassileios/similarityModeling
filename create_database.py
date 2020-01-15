840d3f93cc57a574fe9994edd3a630055953a385
Nikolaus Funk <nikolaus.funk@live.at>
2020-01-15 22:58:01 +0100

    Extracted all necessary colors and updated the database creation with findings

--- create_database.py
+++ create_database.py
@@ -23,11 +23,9 @@ import pandas as pd
 import numpy as np
 import mahotas as mt
 import math
-from PIL import Image
 
 def find_flesh(img):
-	#im = np.array(img.convert('RGB')) #np.array(Image.open("p.png").convert('RGB'))
-	sought = []
+	sought = [[58,95,147],[59,96,148],[88,118,173],[89,119,174],[90,120,175],[91,121,176],[92,122,177],[93,123,178],[94,124,179],[97,122,178],[109,141,194],[110,142,195],[111,143,196],[112,144,197],[113,145,198],[114,146,199],[115,147,200],[116,148,201],[117,149,202],[118,150,203],[119,151,204],[147,191,254],[148,192,255],[150,194,255],[152,196,255]]
 	result = 0
 	for i in range(len(sought)):
 		result += np.count_nonzero(np.all(img==sought[i],axis=2))
@@ -58,7 +56,7 @@ def main():
 	data = prepare('Muppets_03_04_03_pig.txt', 'Muppets-03-04-03/', '.jpg') # data is now an array of pictures with labels
 
 	BATCH_SIZE = 5 # Number of frames that are looked at simulateneously
-	textures = [2,10,11,12]
+	textures = [2,4,7,8,10,12]
 
 	# Optical Flow parameters
 	of_params = dict(winSize = (30,20), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
@@ -111,7 +109,11 @@ def main():
 
 
 def analyse_mp():
-	pictures = ['miss_piggy_1.jpg','miss_piggy_2.jpg','miss_piggy_3.jpg']
+	#pictures = ['miss_piggy_cropped_1.jpg','miss_piggy_cropped_2.jpg','miss_piggy_cropped_3.jpg']
+	pictures = ['miss_piggy_color_1.jpg','miss_piggy_color_2.jpg','miss_piggy_color_3.jpg']
+
+	### TESTING PIG TEXTURES ###
+
 	text = [0,0,0]
 	for i in range(3):
 		image = cv2.imread(pictures[i])
@@ -122,15 +124,47 @@ def analyse_mp():
 		total[i] = math.sqrt((text[0][i]-text[1][i])*(text[0][i]-text[1][i]) + (text[1][i]-text[2][i])*(text[1][i]-text[2][i]) + (text[0][i]-text[2][i])*(text[0][i]-text[2][i]))
 		total[i] /= max(text[0][i],text[1][i],text[2][i])
 	print(total)
-	# Analysis shows that in all three pictures haralick features 2,10,11,12 are almost exactly the same
-	print(text[0][2],text[0][10],text[0][11],text[0][12])
-	print(text[1][2],text[1][10],text[1][11],text[1][12])
-	print(text[2][2],text[2][10],text[2][11],text[2][12])
-	
+	# Analysis shows that in all three pictures haralick features 2,4,7,8,10,12 are almost exactly the same in all samples
+	#print(text[0][2],text[0][4],text[0][7],text[0][8],text[0][10],text[0][12])
+	#print(text[1][2],text[1][4],text[1][7],text[1][8],text[1][10],text[1][12])
+	#print(text[2][2],text[2][4],text[2][7],text[2][8],text[2][10],text[2][12])
+	#image = cv2.imread('Muppets-03-04-03/frame1800.jpg')
+	#print(find_texture(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)))
+	#print(text[0])
 
-analyse_mp()
+	### TESTING COLORS OF PIGS ###
 
-#main()
+	rgb = np.array([[255,255,255]])
+	for i in range(3):
+		image = cv2.imread(pictures[i])
+		rgb = np.concatenate((rgb, image.reshape(-1, image.shape[-1])), axis = 0)
+
+	colors = np.unique(rgb, axis=0, return_counts = True)
+	num_pix = sum(colors[1])
+	pruned_col = [[],[]]
+
+	for i in range(len(colors[1])):
+		if colors[1][i] > 50 and colors[1][i] < 500: # get pink while avoiding white
+			pruned_col[0].append(colors[0][i])
+			pruned_col[1].append(colors[1][i])
+
+	print(pruned_col[1])
+
+	show = np.zeros((1,len(pruned_col[0]),3), np.uint8)
+	for i in range(len(pruned_col[0])):
+		show[0][i][0] = pruned_col[0][i][0]
+		show[0][i][1] = pruned_col[0][i][1]
+		show[0][i][2] = pruned_col[0][i][2]
+
+	cv2.imwrite('pixels.png', show)
+	final = ''
+	for i in range(len(pruned_col[0])):
+		final += '[{},{},{}],'.format(show[0][i][0],show[0][i][1],show[0][i][2])
+	print(final)
+
+#analyse_mp()
+
+main()
 
 
 
