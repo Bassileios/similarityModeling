15fc02058b948c1909ebc65139ff81b023ab0d86
Bassel Mahmoud <dev.bma@outlook.com>
2020-01-17 11:02:24 +0100

    add metrics functions
    Signed-off-by: Bassel Mahmoud <dev.bma@outlook.com>

--- sm1-deep/train.py
+++ sm1-deep/train.py
@@ -177,6 +177,38 @@ def show_batch(image_batch, label_batch, class_names):
         plt.axis('off')
     plt.show()
 
+def precision(y_true, y_pred):
+    """ Precision metric.
+    Only computes a batch-wise average of precision.
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric.
+    Only computes a batch-wise average of recall.
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def f1_score(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return 2*((precision * recall) / (precision+recall + K.epsilon()))
+
 
 if __name__ == '__main__':
     main()
